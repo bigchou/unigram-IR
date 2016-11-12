8781f77076c47f4abd6df109902fca5340cb9da5
bigchou <40347905S@gmail.com>
2016-11-12 15:16:27 +0800

    unknown word & likelihood

--- unigram.py
+++ unigram.py
@@ -31,7 +31,37 @@ def buildDict(corpus):
 
 content = readCorpus("corpus.txt")
 dict = {}
-print(content)
-print(getVocabSize(content))
+#print(content)
+#print(getVocabSize(content))
 dict = buildDict(content)
 print(dict)
+lambd = 0.95
+n = 1000000
+"""
+print("P(nara) = ",lambd * dict["nara"] + (1-lambd) * (1/n))
+print("P(i) = ",lambd * dict["i"] + (1-lambd) * (1/n))
+try:
+	print("P(kyoto) = ",lambd * dict["kyoto"] + (1-lambd) * (1/n))
+except:
+	print("P(kyoto) = ",0.05*(1/n))
+"""
+test_set = readCorpus("test.txt")
+#print(test_set[0].split())
+"""
+prob = 1.0
+for i in test_set[0].split():
+	prob *= (lambd * dict[i] + (1-lambd) * (1/n))
+print(prob)
+"""
+
+# likelihood
+prob = 1.0
+for one in test_set:
+	for i in one:
+		try:
+			prob *= (lambd * dict[i] + (1-lambd)/n)
+		except:
+			prob *= (1-lambd)/n
+print(prob)
+# log likelihood
+
